8e8ccdf22a8a509fd28aa830175a635377f94b59
Evan-Lew <evanml.lew@gmail.com>
2022-12-01 10:49:14 -0800

    added huy's zoom art and lily pad block

--- Assets/Scripts/UIZoomBehavior.cs
+++ Assets/Scripts/UIZoomBehavior.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class UIZoomBehavior : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class UIZoomBehavior : MonoBehaviour
     float orthoSize = 5;
     private bool zoomedIn = true;
     private float speed = 4f;
+    
+    // Sprite variables
+    public Sprite magnifyingIn;
+    public Sprite magnifyingOut;
+    
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,9 +26,19 @@ public class UIZoomBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If statement to switch the sprite images
+        if (zoomedIn)
+        {
+            gameObject.GetComponent<Image>().sprite = magnifyingOut;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = magnifyingIn;
+        }
+        
         foreach(CinemachineVirtualCameraBase virtCam in ((brain.ActiveVirtualCamera as CinemachineStateDrivenCamera).ChildCameras)) {
               (virtCam as CinemachineVirtualCamera).m_Lens.OrthographicSize = Mathf.MoveTowards((virtCam as CinemachineVirtualCamera).m_Lens.OrthographicSize, orthoSize, speed * Time.deltaTime);   
-        } 
+        }
     }
 
     public void zoomInOut()
